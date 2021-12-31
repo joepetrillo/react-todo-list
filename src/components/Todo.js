a8f0126820c3f94f0f97fd02f422f58f42469e13
Joseph Petrillo <jpetrillo119@gmail.com>
2021-12-31 17:47:34 -0500

    add word-break

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import { TiDeleteOutline } from "react-icons/ti";
 const Todo = ({ todo, deleteTodo }) => {
   return (
     <div className="flex justify-between items-center mb-6">
-      <h3 className="text-lg">{todo.task}</h3>
+      <h3 className="text-lg break-all">{todo.task}</h3>
       <button onClick={() => deleteTodo(todo.id)} className="pl-6">
         <TiDeleteOutline size="2rem" />
       </button>
